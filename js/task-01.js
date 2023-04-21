89d08aefb10c980f68a75a5923764e23fd199dae
Vladislavv99 <vladlegur6@gmail.com>
2023-04-22 02:03:16 +0300

    3 taska

---
+++ js/task-01.js
@@ -0,0 +1,18 @@
+// ! ===================== 1 ========================
+
+const numberOfCategories = document.querySelectorAll("#categories .item");
+console.log(`Number of categories: ${numberOfCategories.length}`);
+
+// ! ===================== 1 ========================
+console.log(" ");
+// ! ===================== 2 ========================
+
+numberOfCategories.forEach((categorie) => {
+  const titleEl = categorie.querySelector("h2").textContent;
+  const numOfLi = categorie.querySelectorAll("li").length;
+  console.log(`Category: ${titleEl}`);
+  console.log(`Elements: ${numOfLi}`);
+  console.log(" ");
+});
+
+// ! ===================== 2 ========================
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.getElementById("ingredients");
+
+ingredients.map((el) => {
+  let liEl = document.createElement("li");
+  liEl.textContent = el;
+  liEl.classList.add("item");
+  listEl.append(liEl);
+});
