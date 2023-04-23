22046668ed8e1fd6cdc4298280e2636952550151
Vladislavv99 <vladlegur6@gmail.com>
2023-04-23 12:01:41 +0300

    6 done

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const inputEl = document.querySelector("#validation-input");
+console.log(inputEl.value.length);
+inputEl.addEventListener("input", (event) => {
+  if (event.currentTarget.value.length === 6) {
+    inputEl.classList.remove("invalid");
+    inputEl.classList.add("valid");
+  } else {
+    inputEl.classList.remove("valid");
+    inputEl.classList.add("invalid");
+  }
+  const test = event.currentTarget.value.length;
+  console.log(test);
+});
